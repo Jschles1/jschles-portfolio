2fe163acd4ffb2af2c2e72fc951caea2304faa3b
Jschles1 <johnschlesinger5@gmail.com>
2024-12-01 11:58:30 -0500

    Updates

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -16,7 +16,7 @@ let portfolioData = [
     website: "https://quicknote-app.vercel.app/",
     description: [
       "Mock e-commerce application for audio equipment. Built to mimic the functionality and experience of a real e-commerce site.",
-      "Built with NextJS 13, Typescript, TailwindCSS, Shadcn UI, React Query, Prisma and PostgreSQL.",
+      "Built with Next.js 13, Typescript, TailwindCSS, Shadcn UI, React Query, Prisma and PostgreSQL.",
     ],
   },
   {
@@ -27,19 +27,18 @@ let portfolioData = [
     website: "https://mentormaps.vercel.app/",
     description: [
       "Web application that allows mentors guide mentees towards their career or personal goals through the creation of roadmaps and milestones.",
-      "Built with NextJS 13, Typescript, TailwindCSS, Shadcn UI, React Query, Clerk, Prisma and PostgreSQL.",
+      "Built with Next.js 13, Typescript, TailwindCSS, Shadcn UI, React Query, Clerk, Prisma and PostgreSQL.",
     ],
   },
   {
     id: 3,
     image: image3,
-    title: "Jersey Herb Hub",
+    title: "Garden State Herbhub",
     link: "https://github.com/Jschles1/herbhub-client",
-    website: "https://herbhub-client.vercel.app/",
+    website: "https://www.gardenstateherbhub.com/",
     description: [
-      "Aggregation website for listing all marijuana dispensary products available in New Jersey dispensaries with the ability for users to sort products by dispensary location, product brand, etc.",
-      "Frontend built with NextJS, Typescript, Mantine UI, React Query and Prisma Client.",
-      "Utilizes AWS Lambda, AWS Step Functions and AWS EventBridge scheduler for running Puppeteer web crawlers to retrieve product information from each NJ dispensary website and save it to a MongoDB database on a daily basis.",
+      "Aggregation website for listing all cannabis dispensary products available in New Jersey dispensaries with the ability for users to find and sort products by dispensary location, product brand, price, etc.",
+      "Built with Next.js, Typescript, Mantine UI, React Query and Drizzle ORM, with product data saved to a PostgreSQL database.",
     ],
   },
   {
